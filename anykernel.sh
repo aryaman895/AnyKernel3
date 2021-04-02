696ae3df5254cf9eb5383b6ecff2d1cd438853a7
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2021-04-02 13:35:24 +0000

    AK3 : QuickSilver Kernel Reloaded OS Edition
    * Credits for QS and its hard work goes to my friend @ghostrider_reborn

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=Quicksilver ReloadedOS-Edition by @Aryaman895
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -15,7 +15,7 @@ device.name2=willow
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10.0-11.0
+supported.versions=10.0
 supported.patchlevels=
 '; } # end properties
 
